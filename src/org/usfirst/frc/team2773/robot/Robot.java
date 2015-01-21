f7cb49184457df7fac936a58edd534ff3e23d55a
Peter <Peter@192.168.1.138>
2015-01-20 20:16:07 -0600

    added name to auther's list

--- src/org/usfirst/frc/team2773/robot/Robot.java
+++ src/org/usfirst/frc/team2773/robot/Robot.java
@@ -15,6 +15,7 @@ import edu.wpi.first.wpilibj.Solenoid;
  * @author Darren Midkiff
  * @author Luke Senseney
  * @author Zane Fry
+ * @author Peter Senseney
  */
 public class Robot extends IterativeRobot {
 	RobotDrive drive;
